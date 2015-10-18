f986c9e83509ea1709811cdd2f35f8e290019a0a
osjayaprakash <osjayaprakash@gmail.com>
2015-10-18 04:19:50 -0400

    adding few more files

--- zsh/00-basic.zsh
+++ zsh/00-basic.zsh
@@ -1,10 +1,35 @@
+# Set up the prompt
+
+autoload -Uz promptinit
+promptinit
+prompt fade white
+
+compinit
+
+zstyle ':completion:*' auto-description 'specify: %d'
+zstyle ':completion:*' completer _expand _complete _correct _approximate
+zstyle ':completion:*' format 'Completing %d'
+zstyle ':completion:*' group-name ''
+zstyle ':completion:*' menu select=2
+eval "$(dircolors -b)"
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*' list-colors ''
+zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
+zstyle ':completion:*' matcher-list '' 'm:{a-z}={A-Z}' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=* l:|=*'
+zstyle ':completion:*' menu select=long
+zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
+zstyle ':completion:*' use-compctl false
+zstyle ':completion:*' verbose true
+
+zstyle ':completion:*:kill:*' command 'ps -u $USER -o pid,%cpu,tty,cputime,cmd'
+
+
 ## EDIT COMMANDS
 autoload      edit-command-line
 zle -N        edit-command-line
 bindkey '\ee' edit-command-line
 VISUAL='/usr/bin/vim'
 EDITOR='/usr/bin/vim'
-
 
 #### OTHERS
 COMPLETION_WAITING_DOTS="true"
@@ -22,4 +47,17 @@ zle -N up-line-or-beginning-search
 zle -N down-line-or-beginning-search
 bindkey "^[[A" up-line-or-beginning-search # Up
 bindkey "^[[B" down-line-or-beginning-search # Down
+
+export BITBUCKET=/jp/bb/
+source ${BITBUCKET}/settings/zshrc00
+NODE_NAME=`uname -n | tr '-' '_'`
+OS=`uname -o | tr '/' '_'`
+PDIR=${BITBUCKET}/settings/${NODE_NAME}/${OS}/ 
+mkdir -p "${PDIR}"
+HISTFILE=${PDIR}/zsh_history
+source /jp/venv/app/bin/activate
+
+alias cm="git pull origin master && git add --all /jp/bb/ && git commit -m 'text' && git push origin master"
+alias rt="supervisorctl restart WishDone && service nginx restart"
+
 
