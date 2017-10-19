be7ca4f113f2d24a0002bb636d3a69f4c2089923
Jayaprakash Sundararaj <osjayaprakash@gmail.com>
2017-10-18 17:52:15 -0700

    Update and rename commands.txt to new_machine_setup.sh

---
+++ new_machine_setup.sh
@@ -0,0 +1,31 @@
+set -ex
+
+# CHANGE to HOMe
+cd ~
+
+sudo apt-get install zsh vim tmux git
+which zsh
+which vim 
+which tmux
+which git
+
+# Git setting
+git config --global user.name osjayaprakash
+git config --global user.email osjayaprakash@gmail.com
+git config --global credential.helper 'cache --timeout=86400' # store the password for one day
+
+# CHANGE TO ZSH
+chsh -s $(which zsh)
+
+# USE oh-my-zsh
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+
+# INSTALL DOT FILES
+git clone https://github.com/osjayaprakash/dotfiles/
+echo '' >> ~/.zshrc # append empty line
+echo 'source $HOME/dotfiles/zshrc' >> ~/.zshrc  ## append the link to dotfiles zshrc
+source ~/.zshrc  #zsh_reload
+
+# Tmux 
+sudo rm -rf ~/.tmux.conf
+ln -s $HOME/dotfiles/.tmux.conf ~/.tmux.conf
