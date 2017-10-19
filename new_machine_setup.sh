daa7efb5bddab6d0e94957e1f15fbfc3920de2f4
Jayaprakash Sundararaj <osjayaprakash@gmail.com>
2017-10-18 18:27:18 -0700

    Update new_machine_setup.sh

--- new_machine_setup.sh
+++ new_machine_setup.sh
@@ -24,16 +24,20 @@ chsh -s $(which zsh)
 # Tmux 
 sudo rm -rf ~/.tmux.conf
 ln -s $HOME/dotfiles/tmux.conf ~/.tmux.conf
+ls ~/.tmux.conf
 
 # USE oh-my-zsh if it is not installed already
 test -e $HOME/.oh-my-zsh || wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+test -e $HOME/.oh-my-zsh
 
 # INSTALL DOT FILES ( Keep this for last)
 test -e $HOME/dotfiles || git clone https://github.com/osjayaprakash/dotfiles/
+test -e $HOME/dotfiles
 # append the link to dotfiles zshrc
-tail -n 1 $HOME/.zshrc | grep dotfiles/zshrc || echo '\nsource $HOME/dotfiles/zshrc' >> ~/.zshrc  
+tail -n 1 $HOME/.zshrc | grep dotfiles/zshrc || echo '\nsource $HOME/dotfiles/zshrc' >> ~/.zshrc
+tail -n 1 $HOME/.zshrc | grep dotfiles/zshrc
 # zsh_reload
-source ~/.zshrc 
+# source ~/.zshrc 
 
 # Move to last working directory.
 cd $OLD_PWD
