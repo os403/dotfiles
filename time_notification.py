970b92d1ed75e6d877a28f1467b8a8042eb22c33
Jayaprakash Sundararaj <osjayaprakash@gmail.com>
2019-07-03 21:44:38 -0700

    Update time_notification.py

--- time_notification.py
+++ time_notification.py
@@ -5,6 +5,10 @@ locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')  # Customize
 # Life expectancy is 68 years
 diff = (D(2057, 1, 1) - D.today())
 seconds = int(diff.total_seconds())
+print('Go for walk. Stretch.')
+print('Drink water.')
+print('Deep breath.')
+print('Update the logs.')
 print('{:,} seconds'.format(seconds))
 print('{:,} mins'.format(seconds / 60))
 print('{:,} hours'.format(seconds / 3600))
